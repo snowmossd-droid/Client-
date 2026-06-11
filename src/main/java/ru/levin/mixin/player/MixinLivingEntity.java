dcc078c9e0c6d327c738ba283a7953bc70e2b1da
snowmossd-droid <snowmossd@gmail.com>
2026-06-11 13:22:03 +0700

    Update MixinLivingEntity.java

--- src/main/java/ru/levin/mixin/player/MixinLivingEntity.java
+++ src/main/java/ru/levin/mixin/player/MixinLivingEntity.java
@@ -1,4 +1,5 @@
 package ru.levin.mixin.player;
+
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
@@ -74,43 +75,16 @@ public abstract class MixinLivingEntity implements IMinecraft {
         ci.cancel();
     }
 
-
-    @Inject(method = "calcGlidingVelocity", at = @At("HEAD"), cancellable = true)
-    private void onCalcGlidingVelocity(Vec3d oldVelocity, CallbackInfoReturnable<Vec3d> cir) {
+    @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
+    private void onCalcGlidingVelocity(Vec3d movement, CallbackInfo ci) {
         if (!((Object) this == mc.player)) return;
         AttackAura attackAura = Manager.FUNCTION_MANAGER.attackAura;
         RotationController rotationController = Manager.ROTATION;
         if (attackAura.state && attackAura.correction.get() || rotationController.isControlling()) {
             float customYaw = rotationController.getYaw();
             float customPitch = rotationController.getPitch();
-            Vec3d rotationVec = Vec3d.fromPolar(customPitch, customYaw);
-            float pitchRad = customPitch * (float) (Math.PI / 180F);
-
-            double horizontalLength = Math.sqrt(rotationVec.x * rotationVec.x + rotationVec.z * rotationVec.z);
-            double oldHorizontalLength = oldVelocity.horizontalLength();
-            double gravity = this.getGravity();
-            double cosSquared = MathHelper.square(Math.cos(pitchRad));
-
-            Vec3d newVelocity = oldVelocity.add(0.0, gravity * (-1.0 + cosSquared * 0.75), 0.0);
-
-            if (newVelocity.y < 0.0 && horizontalLength > 0.0) {
-                double lift = newVelocity.y * -0.1 * cosSquared;
-                newVelocity = newVelocity.add(rotationVec.x * lift / horizontalLength, lift, rotationVec.z * lift / horizontalLength);
-            }
-
-            if (pitchRad < 0.0F && horizontalLength > 0.0) {
-                double acceleration = oldHorizontalLength * (-MathHelper.sin(pitchRad)) * 0.04;
-                newVelocity = newVelocity.add(-rotationVec.x * acceleration / horizontalLength, acceleration * 3.2, -rotationVec.z * acceleration / horizontalLength);
-            }
-
-            if (horizontalLength > 0.0) {
-                newVelocity = newVelocity.add((rotationVec.x / horizontalLength * oldHorizontalLength - newVelocity.x) * 0.1, 0.0, (rotationVec.z / horizontalLength * oldHorizontalLength - newVelocity.z) * 0.1);
-            }
-
-            newVelocity = newVelocity.multiply(0.99, 0.98, 0.99);
-
-            cir.setReturnValue(newVelocity);
-            cir.cancel();
+            mc.player.setYaw(customYaw);
+            mc.player.setPitch(customPitch);
         }
     }
 }
