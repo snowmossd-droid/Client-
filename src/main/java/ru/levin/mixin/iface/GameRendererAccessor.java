4ac8f54a2184406420b8e7511ccdc22ef83771b6
snowmossd-droid <snowmossd@gmail.com>
2026-06-11 11:34:36 +0700

    Update GameRendererAccessor.java

--- src/main/java/ru/levin/mixin/iface/GameRendererAccessor.java
+++ src/main/java/ru/levin/mixin/iface/GameRendererAccessor.java
@@ -8,5 +8,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(GameRenderer.class)
 public interface GameRendererAccessor {
     @Invoker("getFov")
-    float invokeGetFov(Camera camera, float tickDelta, boolean changingFov);
+    double invokeGetFov(Camera camera, float tickDelta, boolean changingFov);
 }
